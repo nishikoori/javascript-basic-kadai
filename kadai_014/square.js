0c725b1fd287fa7b846e78e9465a5322fa30b74f
nishikoori <t_nishikoori@yahoo.co.jp>
2024-07-26 11:58:41 +0900

    javascript-basic-kadai

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,7 +1,7 @@
 // 与えられた（num）を二乗し、値を出力する関数を定義する
-const square = (num) => {
-  console.log( num ** 2);
+const calc = (num) => {
+  return (num * num);
 }
 
 // 関数を呼び出し、引数として10を渡す
-square(10);
+console.log(calc(10));
